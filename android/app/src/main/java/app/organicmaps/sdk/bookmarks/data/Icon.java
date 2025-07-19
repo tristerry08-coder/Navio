8b03e990358fb7848b41e621df02b76a7cfa7408
David Martinez <47610359+dvdmrtnz@users.noreply.github.com>
2025-07-19 16:54:06 +0200

    [styles] Add fast_food search result icons & bookmark icons
    Signed-off-by: David Martinez <47610359+dvdmrtnz@users.noreply.github.com>

--- android/app/src/main/java/app/organicmaps/sdk/bookmarks/data/Icon.java
+++ android/app/src/main/java/app/organicmaps/sdk/bookmarks/data/Icon.java
@@ -95,7 +95,8 @@ public class Icon implements Parcelable
       R.drawable.ic_bookmark_none, // ChargingStation
       R.drawable.ic_bookmark_none, // BicycleParking
       R.drawable.ic_bookmark_none, // BicycleParkingCovered
-      R.drawable.ic_bookmark_none  // BicycleRental
+      R.drawable.ic_bookmark_none, // BicycleRental
+      R.drawable.ic_bookmark_none  // FastFood
   };
 
   @PredefinedColor
