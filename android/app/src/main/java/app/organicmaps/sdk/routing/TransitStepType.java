07f745411f22391fe785bc8348a61117a36e50cb
Harry Bond <me@hbond.xyz>
2025-07-31 19:52:33 +0100

    [android] fix drawables not found caused due to the big sdk refactoring - i'm not sure why we're storing some drawables in the sdk package, but this fixes build for now.
    Signed-off-by: Harry Bond <me@hbond.xyz>

--- android/app/src/main/java/app/organicmaps/sdk/routing/TransitStepType.java
+++ android/app/src/main/java/app/organicmaps/sdk/routing/TransitStepType.java
@@ -8,10 +8,10 @@ public enum TransitStepType
   // A specific icon for different intermediate points is calculated dynamically in TransitStepView.
   INTERMEDIATE_POINT(R.drawable.ic_20px_route_planning_walk),
   PEDESTRIAN(R.drawable.ic_20px_route_planning_walk),
-  SUBWAY(R.drawable.ic_route_planning_metro),
-  TRAIN(R.drawable.ic_route_planning_train),
-  LIGHT_RAIL(R.drawable.ic_route_planning_train),
-  MONORAIL(R.drawable.ic_route_planning_monorail),
+  SUBWAY(app.organicmaps.R.drawable.ic_route_planning_metro),
+  TRAIN(app.organicmaps.R.drawable.ic_route_planning_train),
+  LIGHT_RAIL(app.organicmaps.R.drawable.ic_route_planning_train),
+  MONORAIL(app.organicmaps.R.drawable.ic_route_planning_monorail),
   RULER(R.drawable.ic_ruler_route);
 
   @DrawableRes
