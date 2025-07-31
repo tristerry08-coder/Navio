organicmaps/organicmaps

[drape] Allow controlling viewport restoration on DeactivateMapSelection
Signed-off-by: hemanggs <hemangmanhas@gmail.com>

android/app/src/main/cpp/app/organicmaps/Framework.cpp
@@ -621,9 +621,9 @@ void Framework::DeactivatePopup()
   m_work.DeactivateMapSelection();
 }
 
-void Framework::DeactivateMapSelectionCircle()
+void Framework::DeactivateMapSelectionCircle(bool restoreViewport)
 {
-  m_work.DeactivateMapSelectionCircle();
+  m_work.DeactivateMapSelectionCircle(restoreViewport);
 }
 
 /*
@@ -1448,9 +1448,9 @@ Java_app_organicmaps_Framework_nativeDeactivatePopup(JNIEnv * env, jclass)
 }
 
 JNIEXPORT void JNICALL
-Java_app_organicmaps_Framework_nativeDeactivateMapSelectionCircle(JNIEnv * env, jclass)
+Java_app_organicmaps_Framework_nativeDeactivateMapSelectionCircle(JNIEnv * env, jclass, jboolean restoreViewport)
 {
-  return g_framework->DeactivateMapSelectionCircle();
+  return g_framework->DeactivateMapSelectionCircle(restoreViewport);
 }
 
 JNIEXPORT void JNICALL

tristerry08-coder/Navio

android/sdk/src/main/cpp/app/organicmaps/sdk/Framework.cpp
@@ -621,9 +621,9 @@ void Framework::DeactivatePopup()
   m_work.DeactivateMapSelection();
 }
 
-void Framework::DeactivateMapSelectionCircle()
+void Framework::DeactivateMapSelectionCircle(bool restoreViewport)
 {
-  m_work.DeactivateMapSelectionCircle();
+  m_work.DeactivateMapSelectionCircle(restoreViewport);
 }
 
 /*
@@ -1448,9 +1448,9 @@ Java_app_organicmaps_sdk_Framework_nativeDeactivatePopup(JNIEnv * env, jclass)
 }
 
 JNIEXPORT void JNICALL
-Java_app_organicmaps_sdk_Framework_nativeDeactivateMapSelectionCircle(JNIEnv * env, jclass)
+Java_app_organicmaps_sdk_Framework_nativeDeactivateMapSelectionCircle(JNIEnv * env, jclass, jboolean restoreViewport)
 {
-  return g_framework->DeactivateMapSelectionCircle();
+  return g_framework->DeactivateMapSelectionCircle(restoreViewport);
 }
 
 JNIEXPORT void JNICALL