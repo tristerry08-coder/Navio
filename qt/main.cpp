1e358375c6704357c4b0eaaf9bc3a08fd3feb9fe
Harry Bond <me@hbond.xyz>
2025-08-10 14:29:24 +0100

    [desktop] change id to app.comaps.comaps apparently app.comaps.qt is no good either. this one is confirmed fine
    Signed-off-by: Harry Bond <me@hbond.xyz>

--- qt/main.cpp
+++ qt/main.cpp
@@ -133,7 +133,7 @@ int main(int argc, char * argv[])
   UNUSED_VALUE(mainGuard);
 
   QApplication app(argc, argv);
-  app.setDesktopFileName("app.comaps.qt");
+  app.setDesktopFileName("app.comaps.comaps");
   platform.SetupMeasurementSystem();
 
 
